06eae88b56eb135a5ce3c6af5dbaa56ddb785c3b
digitalh2o <digitalh2o@gmail.com>
2018-06-18 22:16:53 -0500

    Archiving test stories successfully and added logger for state change

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
-import { createStore } from "redux";
-//import storyReducer from "../reducers/story";
+import { createStore, applyMiddleware } from "redux";
+import {createLogger } from 'redux-logger';
 import rootReducer from "../reducers";
 
-const store = createStore(rootReducer);
+const logger = createLogger();
+
+const store = createStore(rootReducer, undefined, applyMiddleware(logger));
 
 export default store;
