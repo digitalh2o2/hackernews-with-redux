e2661b827f9a1be9d082e7c1d48b6b84c35f26b6
digitalh2o <digitalh2o@gmail.com>
2018-06-18 23:07:03 -0500

    Grabbing from HN API successfully. Added redux-saga and implemented it within project with no errors.

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Stories from "./Stories";
+import SearchStories from "./SearchStories";
 
 const App = () => (
   <div className="app">
+    <div className="interaction">
+      <SearchStories />
+    </div>
     <Stories />
   </div>
 );
