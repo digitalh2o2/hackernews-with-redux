92f435cdb2a2c9376775a30ab1b313a4dbd27f2d
digitalh2o <digitalh2o@gmail.com>
2018-06-18 19:00:39 -0500

    Initial commit

---
+++ src/components/Stories.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Stories.css";
+
+import Story from "./Story";
+
+const Stories = ({ stories }) => (
+  <div className="stories">
+    {(stories || []).map(story => <Story key={story.objectID} story={story} />)}
+  </div>
+);
+
+export default Stories;
